425d18c95de09a7ba3d560a767f8ba26db249cfe
ZIMsteRr <zimsterr@gmail.com>
2023-07-20 13:44:19 +0300

    todo_list

--- dom/index.js
+++ dom/index.js
@@ -1,1 +1,36 @@
-console.dir(document.children[0].children[1].children[0].textContent)
+//console.dir(document.body.children[0].children[1].children[0].textContent)//
+
+// document.querySelector("#inputComment");
+// document.querySelectorAll();
+
+//console.dir(document.querySelector('.container')) // DOM object
+
+//.closest()
+
+//const liFifthEl = document.querySelector('#liFifthEl')
+//const containerEl = liFifthEl.closest('.container')
+
+//const liSix = document.querySelector('#sixEl')
+//liSix.className = liSix.className.replace('green', 'red')
+
+//const liSix = document.querySelector("#sixEl");
+//iSix.classList.replace("green", "red");
+
+//const liSix = document.querySelector("#sixEl");
+//console.log(liSix.textContent);
+
+//const ul = document.querySelector('ul')
+//const li = document.createElement('li')
+//li.textContent = '7 element'
+//li.classList = 'green'
+//ul.append('li')
+
+//const btn = document.querySelector("#btn");
+
+//btn.addEventListener("click", () => {
+  //console.log("clicked");
+//});
+
+//function onButtonClick() {
+  //document.querySelector("#btn").classList.toggle("red");
+//}
