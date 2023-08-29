0b0ace49af5cc80f4dad04e13e05d95d098a69ad
ZIMsteRr <zimsterr@gmail.com>
2023-08-29 12:04:15 +0300

    node_calc_hw

---
+++ NODEjs/calc_node_hw/index.js
@@ -0,0 +1,9 @@
+const add = require('./calculator/add');
+const sub = require('./calculator/sub');
+const mult = require('./calculator/mult');
+const div = require('./calculator/div');
+
+console.log(add(10, 2));
+console.log(sub(12, 5));
+console.log(mult(7, 4));
+console.log(div(28, 2));
